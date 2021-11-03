a5b63dfae02801a37d9f02d99ba81688b558433f
paulamarv8 <67270157+paulamarv8@users.noreply.github.com>
2021-11-03 19:15:03 +0100

    Update test-lfa.R

--- tests/testthat/test-lfa.R
+++ tests/testthat/test-lfa.R
@@ -1,19 +1,58 @@
-# Test for loss function analysis function
-# 
-# Author: Emilio L. Cano
-###############################################################################
+library(SixSigma)
+library(testthat)
 
+#Loss Function Analysis
 
-lfaNominal <- ss.lfa(ss.data.bolts, "diameter", 
-  lfa.Delta = 0.5, lfa.L0 = 0.001, lfa.Y0 = 10, lfa.output = "text")
-lfaSmaller <- ss.lfa(ss.data.bolts, "diameter", 
-  lfa.Delta = 0.5, lfa.L0 = 0.001, lfa.Y0 = 0, lfa.output = "text")
-lfaLarger <- ss.lfa(ss.data.bolts, "diameter", 
-  lfa.Delta = 0.5, lfa.L0 = 0.001, lfa.Y0 = Inf, lfa.output = "text")
+data <- ss.data.bolts
+cqt <- "diameter"
+delta <- 0.5
+L0 <- 0.001
+Y0 <- 10
+Y0Smaller <- 0
+Y0Larger <- Inf
+output <- "text"
+sub <- "Loss Function Analysis"
 
+lfaNominal <- ss.lfa(data, cqt, delta, Y0, L0, output, sub)
+lfaSmaller <-ss.lfa(data, cqt, delta, Y0Smaller, L0, output, sub)
+lfaLarger <- ss.lfa(data, cqt, delta, Y0Larger, L0, output, sub)
+
+#Test implemented by Emilio L. Cano
 test_that("Constant k is correctly computed",{
-    expect_equal(lfaNominal$lfa.k, 0.001/(0.5^2))
-    expect_equal(lfaSmaller$lfa.k, 0.001/(0.5^2))
-    expect_equal(lfaLarger$lfa.k, 0.001*(0.5^2))
-  })
+  expect_equal(lfaNominal$lfa.k, 0.001/(0.5^2))
+  expect_equal(lfaSmaller$lfa.k, 0.001/(0.5^2))
+  expect_equal(lfaLarger$lfa.k, 0.001*(0.5^2))
+})
 
+####################################################################
+#Unit tests
+#Author: Paula Martinez Vaquero
+####################################################################
+
+test_that("expected data type",{
+  expect_type(data,"list")
+})
+
+test_that("expected data type",{
+  expect_type(cqt,"character")
+})
+
+test_that("expected data type",{
+  expect_type(delta,"double")
+})
+
+test_that("expected data type",{
+  expect_type(L0,"double")
+})
+
+test_that("expected data type",{
+  expect_type(Y0,"double")
+})
+
+test_that("expected data type",{
+  expect_type(output,"character")
+})
+
+test_that("expected data type",{
+  expect_type(sub,"character")
+})
