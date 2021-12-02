75b12315678672fa5be7c44960d40a0093d700a3
paulamarv8 <67270157+paulamarv8@users.noreply.github.com>
2021-12-02 09:17:12 +0100

    Update test-ss-cc.R

--- tests/testthat/test-ss-cc.R
+++ tests/testthat/test-ss-cc.R
@@ -10,9 +10,6 @@ testout[31,] <- list(31,17)
 
 y <- ss.cc("mr", testout, CTQ = "pb.humidity")
 
-climits <- c(0.5,1,2.3)
-z <- ss.cc("mr", ss.data.pb1, CTQ = "pb.humidity", climits=climits)
-
 ####################################################################
 #Unit tests
 #Author: Paula Martinez Vaquero
@@ -32,14 +29,6 @@ test_that("expected data type",{
 
 test_that("length of y",{
   expect_length(y,5)
-})
-
-test_that("expected data type",{
-  expect_type(z,"list")
-})
-
-test_that("length of z",{
-  expect_length(z,5)
 })
 
 test_that("expected data type",{
@@ -74,10 +63,6 @@ test_that("LCL, CL and UCL values from y", {
   expect_equal(y$LCL,0)
   expect_equal(round(y$CL,4),1.7286)
   expect_equal(round(y$UCL,6),5.646528)
-  expect_equal(x$phase,"I")
+  expect_equal(y$phase,"I")
   expect_equal(y$out,30)
-})
-
-test_that("phase is 'II' when cdata or climits are provided",{
-  expect_equal(z$phase,"II")
 })
