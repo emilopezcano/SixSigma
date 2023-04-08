d7b638a1a393afb51fca107a62170d2dc617c89f
emilopezcano <emilopezcano@gmail.com>
2023-04-08 13:03:25 +0200

    Change RData by rda in data files

---
+++ data-raw/Rdata2rda.R
@@ -0,0 +1,30 @@
+## Change RData to rds
+
+dfs <- scan(text = "
+ss.data.batteries                 
+ss.data.bills                     
+ss.data.bolts                     
+ss.data.ca                        
+ss.data.density                   
+ss.data.doe1                      
+ss.data.doe2                      
+ss.data.pastries                  
+ss.data.pb1                       
+ss.data.pb2                       
+ss.data.pb3                       
+ss.data.pb4                       
+ss.data.pc.big                    
+ss.data.pc.r                      
+ss.data.pc                        
+ss.data.rr                        
+ss.data.strings                   
+ss.data.thickness                 
+ss.data.thickness2                
+ss.data.wbx                       
+ss.data.wby", what = character())
+
+for (df in dfs){
+  use_data(assign(df, eval(parse(text = df)), overwrite = TRUE))
+}
+
+use_data(ss.data.bills)
